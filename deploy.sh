44bdece52fc3dfd50b520ca3e6218892bf1c96ad
Ruslan Bryl <rus.bryl@gmail.com>
2021-06-01 19:07:19 +0300

    Some changes

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#-----------Deploy to AWS EC2 instance-----------
+#ansible-playbook -i scripts/ec2.py main.yml
+
+#-----------Deploy to Home Virtual Host---------
+ansible-playbook -i /etc/ansible/hosts main.yml
